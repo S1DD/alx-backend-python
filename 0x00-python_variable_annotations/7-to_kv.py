c0628e7c39c8529d22bb7b153828563e3e907a63
Thabiso <thabisomolefe515@gmail.com>
2025-04-25 23:06:56 +0000

    Corrected return value

--- 0x00-python_variable_annotations/7-to_kv.py
+++ 0x00-python_variable_annotations/7-to_kv.py
@@ -1,9 +1,9 @@
 #!/usr/bin/ env python3
-
+'''Task 7: Complex types - string and int/float to tuple
+'''
 from typing import  Union, Tuple
 
 
 def to_kv(k: str, v: Union[int, float]) -> Tuple[str, float]:
      """Converts a Python variable to a KV pair."""
-     return k, v ** 2    
-
+     return (k, float(v ** 2))   
