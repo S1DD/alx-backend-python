658ba495dcff7262da5e8275d216f8c808a2ebe0
Thabiso <thabisomolefe515@gmail.com>
2025-04-25 22:54:55 +0000

    Added comments

--- 0x00-python_variable_annotations/0-add.py
+++ 0x00-python_variable_annotations/0-add.py
@@ -1,4 +1,9 @@
-#!/usr/bin/python3
+#!/usr/bin/env python3
+'''Task 0: Basic annotations - add
+'''
+
 
 def add(a: float, b: float) -> float:
+    '''Adds two floating-point numbers.
+    '''
     return a + b
