d1b28de9be5894147b7ea7a05182383b59102b8d
Thabiso <thabisomolefe515@gmail.com>
2025-04-29 20:14:08 +0000

    Async solutions

---
+++ 0x01-python_async_function/1-concurrent_coroutines.py
@@ -0,0 +1,22 @@
+#!/usr/bin/env python3
+
+"""Contains a method that spawns wait_random n times with a
+specified delay between each call."""
+
+from typings import List
+import asyncio
+
+wait_random = __import__('0-basic_async_syntax').wait_random
+
+
+async def wait_n(n: int, max_delay: int) -> List[float]:
+    """Spawns wait_random n times with a specified delay
+    between each call.
+    Args:
+        n: number of times to spawn wait_random
+        max_delay: maximum delay between each call
+    Returns:
+        list of delays
+    """
+    tasks = [asyncio.create_Task(wait_random(max_delay) for i in range(n)]
+    return [await task for task in asyncio.as_completed(tasks)]
