2e98daf027d6f2ee748474460168d56e9dca2e95
Thabiso <thabisomolefe515@gmail.com>
2025-05-22 17:23:06 +0200

    Fixed indenation issues

--- 0x03-Unittests_and_integration_tests/test_utils.py
+++ 0x03-Unittests_and_integration_tests/test_utils.py
@@ -42,6 +42,7 @@ class TestAccessNestedMap(unittest.TestCase):
         with self.assertRaises(KeyError):
             access_nested_map(nested_map, path)
 
+
 class TestGetJson(unittest.TestCase):
     '''Tests the `utils.get_json` function.
     '''
